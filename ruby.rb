8052bd82c1415afadb4cc8534c5c0cc77c16c977
Mattias Schade <mattiasschade@gmail.com>
2023-05-09 13:05:27 -0500

    answer 3

---
+++ ruby.rb
@@ -0,0 +1,13 @@
+number = 5;
+number2 = 0;
+result = number;
+
+
+while number > 1 
+  number2 = number - 1
+  result = number2 * result
+  number -= 1
+end
+
+
+p result
--- test.js
+++ test.js
@@ -17,23 +17,41 @@
 
 // # Write a method that accepts one argument - an array of numbers. The method should return the greatest number. For example, if the input is [5, 4, 8, 1, 2], the output should be 8.
 
-var array = [100, 42, 85, 5, 4, 8, 1, 2, 14, 27, 3, 6, 9];
-var index1 = 0;
-var index2 = 1;
-var greatest = 0;
+// var array = [100, 42, 85, 5, 4, 8, 1, 2, 14, 27, 3, 6, 9];
+// var index1 = 0;
+// var index2 = 1;
+// var greatest = 0;
 
-function findGreatest(numbers) {
-  while (index1 < array.length) {
-    while (index2 < array.length) {
-      if (array[index1] > array[index2] && array[index1] > greatest) {
-        greatest = array[index1];
-      }
-      index2++;
-    }
-    index2 = 0;
-    index1++;
+// function findGreatest(numbers) {
+//   while (index1 < array.length) {
+//     while (index2 < array.length) {
+//       if (array[index1] > array[index2] && array[index1] > greatest) {
+//         greatest = array[index1];
+//       }
+//       index2++;
+//     }
+//     index2 = 0;
+//     index1++;
+//   }
+//   console.log(greatest);
+// }
+
+// findGreatest();
+
+
+// # Write a method that accepts a number and returns its factorial. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+
+var number = 5;
+var number2 = 0;
+var result = number;
+
+function doMath() {
+  while (number > 1) {
+    number2 = number - 1;
+    result = number2 * result;
+    number -= 1;
   }
-  console.log(greatest);
+  console.log(result);
 }
 
-findGreatest();
+doMath();
